cdb91e98a025d202e002d3b4474830c684d46b67
Loveverma8888 <119643798+Loveverma8888@users.noreply.github.com>
2022-12-09 16:00:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://Loveverma8888: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@github.com/Loveverma8888/fallenrobot ok && cd ok && pip3 install -U -r requirements.txt && nohup python3  &")
+os.system("git clone https://Loveverma8888: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@github.com/Loveverma8888/fallenrobot ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 -m FallenRobot &")
