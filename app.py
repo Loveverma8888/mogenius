de05668c6637d638669b3ce4d532692b9225f399
Loveverma8888 <119643798+Loveverma8888@users.noreply.github.com>
2022-12-09 15:56:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://username:token@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://Loveverma8888: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@github.com/Loveverma8888/fallenrobot ok && cd ok && pip3 install -U -r requirements.txt && nohup python3  &")
